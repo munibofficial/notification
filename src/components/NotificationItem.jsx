f64a5d9d10a1b046fb2c2c09b7e11cad6a7e3931
munibofficial <muneebamjad.official@gmail.com>
2023-08-06 12:59:25 +0500

    updated time

--- src/components/NotificationItem.jsx
+++ src/components/NotificationItem.jsx
@@ -1,8 +1,12 @@
 import React from "react";
+import moment from 'moment';  // Add this line
 import ReadUnreadIndicator from "./ReadUnreadIndicator";
 
 const NotificationItem = ({notification}) => {
     const itemClass =  notification.read? 'notification-item' : 'notification-item-uread';
+    const timeElapsed = (date) => {
+        return moment(date).fromNow(); 
+    };
     return(
         <div className={itemClass}>
             <div className="item-container">
@@ -14,7 +18,7 @@ const NotificationItem = ({notification}) => {
             {notification.picture && 
              <img style={{width:"30px" , height:"30px" ,marginLeft:"auto",marginTop:"auto"}} src={notification.picture}/> }
             </div>
-            <p style={{marginLeft:"20px",marginTop:"-16px" ,fontSize:"8px" ,padding:"inherit",}}>5 days ago</p>
+            <p style={{marginLeft:"20px",marginTop:"-16px" ,fontSize:"8px" ,padding:"inherit",}}>{timeElapsed(notification.time)}</p>
             {notification.message && 
                 <div className="message-container">
                     <p style={{padding:"10px" ,maxWidth: "80%", margin: "0 auto", fontSize:"10px", textAlign:"left",borderRadius:"5px", backgroundColor:"hsl(210, 60%, 98%)"}}>{notification.message}</p>
--- src/components/NotificationPage.jsx
+++ src/components/NotificationPage.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState , useEffect } from 'react';
+import moment from 'moment'; 
 import NotificationList from './NotificationList';
 import NotificationCounter from './NotificationCounter';
 import MarkAllAsRead from './MarkAllAsRead';
@@ -20,13 +21,24 @@ const NotificationPage = () => {
         { id: 2, avatar: avatar1,name: 'Angela Gray', content: 'followed you', read: false },
         { id: 3, avatar: avatar3,name: 'Jackob Thompson', content: 'has joined your group',title: 'Chess Group', read: true },
         { id: 4, avatar: avatar6,name: 'Rizky Hasanuddin', content: 'sent you private message', message: "is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it" ,  read: false },
-        { id: 5, avatar: avatar5,name: 'Kimberly Smith', content: 'commented on your picture',picture: comment, read: false },
+        { id: 5, avatar: avatar5,name: 'Kimberly Smith', content: 'commented on your picture',picture: comment, read: false,time: new Date() },
         { id: 6, avatar: avatar7,name: 'Nathan Peterson', content: 'reacted to your recent post',title: '5 end game strategies' , read: false },
         { id: 6, avatar: avatar8,name: 'Anna Kim', content: 'left the group',title: 'Chess Club' , read: false },
 
 
 
     ]);
+    const timeElapsed = (date) => {
+        return moment(date).fromNow(); // This will return a string like "5 minutes ago", "2 hours ago", etc.
+    };
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setNotifications(notifications => [...notifications]);
+        }, 60000); // Update the component every minute to refresh the time ago string
+        return () => clearInterval(interval);
+    }, []);
+
     const markAllAsRead = () => {
         const updatedNotifications = notifications.map(notification => {
             return { ...notification, read: true };
